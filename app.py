89936af0e90c93fbab2d3188614094e8f1bc640a
Isaac Lin <me@leolin.cn>
2019-04-27 17:31:52 +0800

    Reach page 50.

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+import os
+import click
+from flask import Flask, request, make_response, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY', 'NoSecretKey:-(')
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello():
+    name = request.args.get('name')
+    if name is None:
+        # Get the cookies stored in the session.
+        name = session.get('name', 'Blanker000')
+    return 'Hello, {name}!'.format(name=name)
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/set/<name>')
+def set_cookie(name):
+    response = make_response(redirect(url_for('hello')))
+    response.set_cookie('name', name)
+    return response
+
+
+@app.route('/login')
+def login():
+    session['logged_in'] = True
+    session['name'] = 'Blanker111'
+    print(session.get('name'))
+    return redirect(url_for('hello'))
